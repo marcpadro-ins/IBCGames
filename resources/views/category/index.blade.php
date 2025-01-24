d9efa3eae771c9bcef4e80804f2db2322e258f7a
Marc Padró Busquets <marc.padro@insbaixcamp.cat>
2025-01-24 12:38:08 +0100

    Update index.blade.php

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,12 +1,22 @@
 @extends('layout')
 
 @section('content')
-    <h1 class="mb-4">Categories</h1>
-    <div class="list-group">
+<div class="container mt-5">
+    <h1 class="display-4 text-center mb-4">Categories</h1>
+    
+    <div class="row">
         @foreach($categories as $category)
-            <a href="{{ route('category.show', $category->id) }}" class="list-group-item list-group-item-action">
-                {{ $category->name }}
-            </a>
+            <div class="col-md-6 col-lg-4 mb-3">
+                <div class="card shadow-sm">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">{{ $category->name }}</h5>
+                        <a href="{{ route('category.show', $category->id) }}" class="btn btn-primary w-100">
+                            Veure més
+                        </a>
+                    </div>
+                </div>
+            </div>
         @endforeach
     </div>
+</div>
 @endsection
